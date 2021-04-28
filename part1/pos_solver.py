034d520e1bb3b07b8b6b85793867f792ba3933b7
Vijay Iyer <vsiyer@iu.edu>
2021-04-28 11:00:44 -0400

    Naive Bayes - Simplified Problem 1

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -9,7 +9,8 @@
 
 import random
 import math
-
+import collections
+from collections import defaultdict
 
 # We've set up a suggested code structure, but feel free to change it. Just
 # make sure your code still works with the label.py and pos_scorer.py code
@@ -31,18 +32,56 @@ class Solver:
     # Do the training!
     #
     def train(self, data):
-        pass
+        self.p_words = {}
+        self.p_pos = {}
+        self.pos_word_pos = {}
+        self.total_words = 0
+        self.total_pos = 0
+        for i, (words, tags) in enumerate(data):
+            for word, tag in zip(words,tags):
+                if word not in self.p_words:
+                    self.p_words[word] = {}
+                    self.p_words[word]['total_count'] = 1
+                    if tag not in self.p_pos:
+                        self.p_pos[tag] = 1
+                    else:
+                        self.p_pos[tag] += 1
+                    if tag not in self.p_words[word].keys():
+                        self.p_words[word][tag] = 1
+                    # self.p_words[word]['total_count'] = 1
+                else:
+                    self.p_words[word]['total_count'] += 1
+                    if tag not in self.p_words[word].keys():
+                        self.p_words[word][tag] = 1
+                    else:
+                        self.p_words[word][tag] += 1
+                self.total_words += 1
+                self.total_pos += 1
 
     # Functions for each algorithm. Right now this just returns nouns -- fix this!
     #
     def simplified(self, sentence):
-        return [ "noun" ] * len(sentence)
-
+        output_pos = []
+        for word in sentence:
+            output = {k: 0 for k in self.p_pos.keys()}
+            if word not in self.p_words:
+                for pos in self.p_pos.keys():
+                    output[pos] = (1/(self.p_pos[pos]+self.total_words))
+            else:
+                p_word = self.p_words[word]['total_count']/self.total_words
+                for pos in self.p_pos.keys():
+                    if pos not in self.p_words[word].keys():
+                        output[pos] = 0
+                    else:
+                        answer = (self.p_words[word][pos]/ self.p_pos[pos]) * (self.p_pos[pos]/self.total_pos)
+                        output[pos] = answer
+            output_pos.append(max(output, key = lambda t:output[t]))
+        return output_pos
     def hmm_viterbi(self, sentence):
-        return [ "noun" ] * len(sentence)
+        return [random.choice(["noun","verb","adj","pron","det"]) for word in range(len(sentence))]
 
     def complex_mcmc(self, sentence):
-        return [ "noun" ] * len(sentence)
+        return [random.choice(["noun","verb","adj","pron","det"]) for word in range(len(sentence))]
 
 
 

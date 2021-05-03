0dd5f70b0d4feb4fef5071ba749b3fbd80408853
Vijay Iyer <vsiyer@iu.edu>
2021-05-02 22:12:35 -0400

    Complex MCMC Gibbs Sampling Code

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -10,6 +10,8 @@
 import random
 import math
 import numpy as np
+from numpy import random
+import itertools
 import collections
 from collections import defaultdict
 import itertools
@@ -95,6 +97,7 @@ class Solver:
         self.pos_word_pos = {}
         self.total_words = 0
         self.total_pos = 0
+
         for i, (words, tags) in enumerate(data):
             for word, tag in zip(words,tags):
                 if word not in self.p_words:
@@ -119,6 +122,17 @@ class Solver:
                         self.p_words[word][tag] += 1
                 self.total_words += 1
                 self.total_pos += 1
+
+        # joint probability distribution for each word given the pos tag before and current pos tag - required for complex bayes net gibbs sampling
+        self.pos_word_pos1_pos2 = {word:{(k[0], k[1]): 1 for k in itertools.product(self.p_pos.keys(), repeat=2)} for word in self.p_words.keys()}
+        for sentence in data:
+            for j in range(len(sentence[0])):
+                self.pos_word_pos1_pos2[sentence[0][j]][(sentence[1][j-1],sentence[1][j])] += 1
+        # normalizing for later use
+        # for word in self.pos_word_pos1_pos2.keys():
+        #
+        #     self.pos_word_pos1_pos2[word] = {(k[0],k[1]):self.pos_word_pos1_pos2[word][k[0],k[1]]/sum(self.pos_word_pos1_pos2[word].values())
+        #                                       for k in self.pos_word_pos1_pos2[word].keys()}
 
         # Transition probabilities
         self.trans_prob = {k:{} for k in self.p_pos.keys()}
@@ -197,7 +211,58 @@ class Solver:
         return pos_path[::-1]
 
     def complex_mcmc(self, sentence):
-        return [random.choice(["noun","verb","adj","pron","det"]) for word in range(len(sentence))]
+        initial_state = [random.choice(list(self.p_pos.keys())) for _ in range(len(sentence))]
+        possible_sequences = {}
+        pos_values_sum = sum(self.p_pos.values())
+        word_pos_sum = {k:0 for k in self.p_words.keys()}
+        p_init_prob_renormalized = {k:0 for k in self.init_prob.keys()}
+        for k in p_init_prob_renormalized.keys():
+            p_init_prob_renormalized[k] = self.init_prob[k]/sum(self.init_prob.values())
+        k = 1000  # no. of samples to take
+        break_in = 500
+        for word in self.p_words.keys():
+            word_pos_sum[word] = sum(self.p_words[word].values())
+        for i in range(k):
+            current = initial_state
+            for sample_ind in range(len(sentence)):
+
+                # for first word, POS tag only based on the first word itself, this is passed in as the probability distribution
+                if sample_ind == 0:
+                    current[sample_ind] = np.random.choice(list(self.p_pos.keys()),p=list(p_init_prob_renormalized.values()))
+                # for 2nd word
+                else:
+                    prob_dist = {k:0 for k in self.p_pos.keys()}
+                    for k in self.p_pos.keys():
+                        # if word itself is new, then give each POS tag equal probability for that word
+                        '''
+                        in order to model as per the complex bayes net model we need to use - 
+                        - pos_word_pos1_pos2[word][(known pos tag,<tag>)] instead of self.p_words[word][<tag>]
+                         for every possible tag and build the table this way. however, for some reason, the probability 
+                         values are too low for the pos_word_pos1_pos2[word][(known pos tag,<tag>)] (this is the probability 
+                         of word occuring given s2 and s1
+                         '''
+
+                        if sentence[sample_ind] not in self.p_words.keys():
+                            prob_dist[k] = 1/(self.p_pos[k])*(self.trans_prob[current[sample_ind-1]][k])*(self.p_pos[current[sample_ind-1]]/pos_values_sum)
+                        else:
+                            if k not in self.p_words[sentence[sample_ind]].keys():
+                                prob_dist[k] = (1/self.total_words)*(1/sum(self.p_words[sentence[sample_ind]].values()))*(self.p_pos[current[sample_ind-1]]/pos_values_sum)
+                            else:
+                                prob_dist[k] = (self.pos_word_pos1_pos2[sentence[sample_ind]][(current[sample_ind-1],k)]) * (self.trans_prob[current[sample_ind - 1]][k]) * (
+                                                           self.p_pos[current[sample_ind - 1]] / pos_values_sum)
+                                # prob_dist[k] = (self.p_words[sentence[sample_ind]][k]/word_pos_sum[sentence[sample_ind]])*(self.trans_prob[current[sample_ind-1]][k])*(self.p_pos[current[sample_ind-1]]/pos_values_sum)
+
+                    prob_dist_sum = sum(prob_dist.values())
+                    for k in self.p_pos.keys():
+                        prob_dist[k] = prob_dist[k]/prob_dist_sum
+                    current[sample_ind] = np.random.choice(list(self.p_pos.keys()),p=list(prob_dist.values()))
+                if i > break_in:
+                    if tuple(current) not in possible_sequences.keys():
+                        possible_sequences[tuple(current)]=1
+                    else:
+                        possible_sequences[tuple(current)]+=1
+
+        return list(max(possible_sequences, key=possible_sequences.get))
 
 
 
